f91cdaba2354fc6a1fdf80a79764cf0e957a87f5
maivinhlam <maivinhlam910@gmail.com>
2020-12-21 10:30:25 +0700

    Add UserController, group admin page, edit name of migrate user

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('user', 'UserController');
-Route::resource('product', 'ProductController');
+Route::prefix('admin')->group(function () {
+    Route::resource('users', 'UserController');
+    Route::resource('products', 'ProductController');
+});
