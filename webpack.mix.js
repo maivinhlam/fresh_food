5699618e1febe202e1ec8b2c199422dc19be9fe6
maivinhlam <maivinhlam910@gmail.com>
2021-01-13 21:54:13 +0700

    Move file css,js from folder public to folder resource (2)

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/script.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')    
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/AdminLTE-3.0.5/AdminLTE.scss', 'public/css')
+    .copyDirectory('resources/plugins', 'public/plugins')
+    .copyDirectory('resources/css', 'public/css')
+    .copyDirectory('resources/js', 'public/js')
+    ;
 
