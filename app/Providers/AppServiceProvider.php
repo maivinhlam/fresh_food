83889e62b1a42c7f5d85b5f9e6b3a8c7c6656188
maivinhlam <maivinhlam910@gmail.com>
2021-03-15 09:48:11 +0700

    Fix error encrypt when startserve

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 use App\Models\Cart;
 use App\Models\CartDetail;
@@ -33,13 +34,17 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         $cart_count = 0;
+        $cartID = null;
         if (Auth::check()) {
             $cart = Cart::where('user_id', Auth::user()->id)->first();
             $cart_count = $cart->details->count();
         } else {
-
-            $cartID = Crypt::decryptString(Cookie::get('cartID'));
-            $cartID = explode('|', $cartID)[1];
+            try {
+                $cartID = Crypt::decrypt(Cookie::get('cartID'), false);
+                $cartID = explode('|', $cartID)[1];
+            } catch (DecryptException $e) {
+                $cartID = 0;
+            }
 
             $cart = Cart::find($cartID);
             if($cart) $cart_count = $cart->details->count();
